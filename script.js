cef63358298f9b72f4eb285b45fe7097fdb0617e
Anastasia Krukovich <ananastasia.krukovich@gmail.com>
2023-02-05 22:51:49 +0100

    Выполнение д/з
    Поработала над файлом script.js, соблюла все условия в комментариях

--- script.js
+++ script.js
@@ -7,3 +7,26 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector('#input');
+const productsContainer = document.querySelector('.groceries');
+
+input.addEventListener('keydown', function(event) {
+  if (event.key == 'Enter') {
+    const productName = input.value;
+
+    const newProduct = document.createElement('div');
+    newProduct.classList.add('items');
+    newProduct.textContent = productName;
+
+    if (input.value != '') {
+      productsContainer.append(newProduct);
+    }
+
+    newProduct.addEventListener('click', function() {
+      newProduct.classList.toggle('done');
+    })
+
+    input.value = '';
+  }
+})
